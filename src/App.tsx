50e53381d4b9ae4529322ebc036f5e65dc047a4d
shirokuma2 <devlop.milano2022@gmail.com>
2022-02-23 16:08:38 +0900

    前半完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import TaskList from './components/TaskList';
+import TaskInput from './components/TaskInput';
+import { Task } from './Types';
 import './App.css';
 
-function App() {
+const initialState: Task[] = [
+  {
+    id: 2,
+    title: "次のtodo",
+    done: false
+  },
+  {
+    id: 1,
+    title: "最初のtodo",
+    done: true
+
+  }
+]
+
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState(initialState)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <TaskInput />
+     <TaskList tasks={tasks} />
+   </div>
   );
 }
 
---
+++ src/components/TaskInput.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+
+const TaskList: React.FC = () => {
+const [inputTitle, setInputTitle] = useState("")
+
+const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  setInputTitle(e.target.value)
+}
+  return (
+    <div className='input-form'>
+      <div className='inner'>
+        <input 
+          type="text"
+          className='input'
+          value={inputTitle}
+          onChange={handleInputChange}
+         />
+         <button className='btn is-primary'>追加</button>
+      </div>
+    </div>
+  )
+}
+
+export default TaskList;
